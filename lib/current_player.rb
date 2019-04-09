1fbe92f55db3939f9d6a724143587003496a2276
James Declerus <james.declerus@esih.edu>
2019-04-09 13:11:50 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+
+#definition of the function tur count
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+  end
+end
+return counter
+end
+
+#definition of function current player
+def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
